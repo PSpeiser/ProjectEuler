55fbfb8efb7de3cc1690483c9d7fd18b829849c4
PSpeiser <mail@patrickspeiser.at>
2014-02-14 15:30:06 +0100

    Problem 3 Solved

---
+++ Problem 3.py
@@ -0,0 +1,14 @@
+import math
+def rwh_primes(n):
+    # http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
+
+n = 600851475143
+candidates = rwh_primes(int(math.sqrt(n)))
+prime_factors = [prime for prime in candidates if n % prime == 0]
+print max(prime_factors)
